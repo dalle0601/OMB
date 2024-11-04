fca5810c4e3e8537d1ef2958b3147fec7735f650
dalle0601 <dalle0601@gmail.com>
2024-11-04 23:51:55 +0900

    [init] project init

---
+++ src/main/java/com/project/omb/OmbApplication.java
@@ -0,0 +1,13 @@
+package com.project.omb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/omb/OmbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.omb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
